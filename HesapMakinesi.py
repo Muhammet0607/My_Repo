20afbf5bf9209c7274003ce98a577559a80fb3dd
Muhammet <mgurbu4545a@gmail.com>
2026-04-02 20:21:00 +0300

    HesapKod

---
+++ HesapMakinesi.py
@@ -0,0 +1,15 @@
+def F1():
+    a = float(input("İlk sayı: "))
+    b = float(input("ikinci sayı: "))
+
+    toplama = a+b
+    çıkarma = a-b
+    çarpma = a*b
+    bölme = a/b
+
+    print("toplama sonucu: ", toplama)
+    print("çıkarma sonucu: ", çıkarma)
+    print("bölme sonucu: ", bölme)
+    print("çarpma sonucu: ", çarpma)
+
+F1()
