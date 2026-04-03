10096646e9e7ac12588fafb0cc29916aed93e934
Muhammet <mgurbu4545a@gmail.com>
2026-04-03 22:37:53 +0300

    tahmin

---
+++ SayıTahminEtme.py
@@ -0,0 +1,12 @@
+import random   
+
+r1 = random.randint(1, 3)
+
+    
+tahmin = input("1 ile 10 arasında bir sayı tahmin edin: ")
+print("sayı: ", r1)
+
+if tahmin == r1:
+    print("Doğru Tahmin");
+    
+
